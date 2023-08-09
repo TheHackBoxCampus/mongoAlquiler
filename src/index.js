8de56d5955a0f06a72c5c2077426627d4e0e0cf7
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-09 09:26:05 -0500

    feat(Root): Ejecute conx
    La funcion conx realiza la conexion al cloud de mongo db atlas

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import app from "./app.js";
 import globalProperties from "./env/env.js";
+import conx from "./config/db.js";
 
-const start = () => {
+let stable = false
+
+const start = async () => {
     try {
+        stable = await conx(); 
         const $server = JSON.parse(globalProperties.SERVER); 
         app.listen($server, () => console.log(`listening http://${$server.hostname}:${$server.port}`)); 
     }catch(err) {
-        throw Error(err)
+        console.log(err)
     }
 }
 
-start(); 
+start();    
+export default stable; 
