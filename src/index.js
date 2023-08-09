b4254e4e0f345462b653a58ba3f817e508eaaea2
OscarAbril09 <osnan.a9@hotmail.com>
2023-08-09 15:22:09 -0500

    refactor(Root): Espacio Se añadio el espacio para el reconocimiento de las llaves

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ let stable = false
 
 const start = async () => {
     try {
-        stable = await conx(); 
-        const $server = JSON.parse(globalProperties.SERVER); 
-        app.listen($server, () => console.log(`listening http://${$server.hostname}:${$server.port}`)); 
-    }catch(err) {
+        stable = await conx();
+        const $server = JSON.parse(globalProperties.SERVER);
+        app.listen($server, () => console.log(`listening http://${$server.hostname}:${$server.port}`));
+    } catch (err) {
         console.log(err)
     }
 }
 
-start();    
+start();
 export default stable; 
