176efac7a9c7d5c82415b2fcabdb2bbb51dc8ebd
OscarAbril09 <osnan.a9@hotmail.com>
2023-08-09 15:23:17 -0500

    feat(Root): Configuracion de rutas
    Se añadio la ruta de auth para el usuario y se importaron dependencias
    
    - cors
    - morgan
    - ruta => authUserRoute

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
-import express from "express"; 
+import express from "express";
+import morgan from "morgan";
+import cors from "cors"
+import authUserRoute from "./routes/auth.user.js";
 
-const app = express(); 
-
+const app = express();
 
 /**
  * ! middleware
  */
 app.use(express.json());
-app.use(express.text()); 
+app.use(express.text());
+app.use(morgan("dev"));
+app.use(cors());
 
+
+/**
+ * * Routes
+ */
+
+app.use(authUserRoute);
 
 export default app; 
