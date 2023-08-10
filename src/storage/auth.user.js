59eca85fa417a844fe2816376b6b1c53eaaf16c3
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-10 00:08:18 -0500

    feat(Root): Datos escapados
    Se hizo la funcion que escapara los datos antes de que validara su contenido

--- src/storage/auth.user.js
+++ src/storage/auth.user.js
@@ -1,25 +1,36 @@
 class user {
     constructor(name, password, email) {
+        this.args = arguments 
         this.name = name;
         this.password = password,
         this.email = email
     }
 
-    validateData() {
-        const schema = {
+    scapeData() {
+        let schema = {
             name: this.name,
             password: this.password,
             email: this.email
         }
+        let state = Boolean();  
 
-        for (let [key, value] in schema) {
-            let count = 0;
-            
-            if (count > 3 || count < 3) return "Cantidad de registros incorrecto!!"
+        let typeData = Object.keys(schema).reduce((at, item) => {
+            if(typeof schema[item] != "string") {
+                at = Object.entries(schema).length; 
+                state = true
+            }
+            if(this.args.length > Object.entries(schema).length || this.args.length < 3) state = true
 
-            if (typeof value != "string") return "Contenido de los registros incorrecto!!"
-     
-            count++; 
-        }
+            return state ? "parametros incorrectos" : null
+        }, 0)
+        
+
+         let redirect = [typeData].map(err => err != null ? err : schema );      
+         
+         return redirect
     }
 }
+
+
+let inst = new user("miller", "false", 1); 
+console.log(inst.scapeData()); 
