e5a93e650f2b5554426c0077f9ca56c607b6b30a
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-10 09:33:28 -0500

    feat(Root): Validaciones
    se agregaron algunas validaciones para configurar el dto y escapar datos

--- src/storage/auth.user.js
+++ src/storage/auth.user.js
@@ -24,13 +24,28 @@ class user {
             return state ? "parametros incorrectos" : null
         }, 0)
         
-
          let redirect = [typeData].map(err => err != null ? err : schema );      
          
          return redirect
     }
+    
+    validateContentData([obj] = null) {
+        let arrx = [/^[a-z A-Z 0-9]*$/, /\S+@(\S+\.\S+)/]
+        let obje = {name: "jorge5", email: "kalednarinogmail.com"}
+        let result = ""
+
+        for(let i = 0; i < arrx.length; i++) {
+            for (let prop in obje) {
+                result += arrx[i].test(obje[prop])
+                i += 1
+            }
+        }
+        return result ;
+    }
 }
 
 
-let inst = new user("miller", "false", 1); 
-console.log(inst.scapeData()); 
+let inst = new user("miller", "false", "kalednarino@gmail.com"); 
+let filter1 = inst.scapeData(); 
+let filter2 = inst.validateContentData(filter1); 
+console.log(filter2)
