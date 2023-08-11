c27340b29bee9f1acc3d5f14a5c29255c5d603c0
TheHackBoxCampus <millercampusdeveveloper@gmail.com>
2023-08-11 09:16:08 -0500

    feat(Root): Añadi logica controlador
    añadi la logica al controlador para que genere el token y verfique el usuario

--- src/controllers/auth.user.controller.js
+++ src/controllers/auth.user.controller.js
@@ -1,12 +1,40 @@
-import stable from "../index.js";
+import generateToken from "../libs/generateKey.js";
+import globalProperties from "../env/env.js";
+import axios from "axios";
+import login_md from "../storage/auth.user.login.js";
 
-const login = (req, res) => {
-    const credential = req.body;
-
-    
-
+const gu = async () => {
+    let $server = JSON.parse(globalProperties.SERVER);
+    try {
+        let c = await axios.get(`http://${$server.hostname}:${$server.port}/users`)
+        let u = await c.data; 
+        return u; 
+    }catch (err) {
+        console.log(err.message)
+    }
 }
 
-export {
-    login
+const login = async (req, res) => {
+    let d = req.body; 
+    let t = await generateToken(); 
+    let u = await gu(); 
+    let ru = []; 
+
+    for(let p in u) {
+        for(let x=0; x < u[p].length; x++){
+            ru.push(u[p][x])
+        }
+    }
+
+    let a = false; 
+
+    let nu = new login_md()
+
+    for(let g=0; g < ru.length; g++) {
+        ru[g].name == d.name && ru[g].password == d.password ? a = !a : a = a;
+    }
+
+    a ? res.send({user:d, token:t}) : res.status(401).send("usuario no encontrado");
 }
+
+export { login }
