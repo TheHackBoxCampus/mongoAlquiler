36a029078872215bd1205336867fe5ce03575a9d
TheHackBoxCampus <millercampusdeveveloper@gmail.com>
2023-08-11 09:09:51 -0500

    feat(Root): archivos estaticos
    servi el archivo users.json para la verificacion con el token

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors"
 import authUserRoute from "./routes/auth.user.js";
+import path from "path"
 
 const app = express();
 
@@ -13,6 +14,12 @@ app.use(express.text());
 app.use(morgan("dev"));
 app.use(cors());
 
+/**
+ * !json
+ */
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+app.use("/users", express.static(path.join(__dirname, "users/users.json")))
 
 /**
  * * Routes
