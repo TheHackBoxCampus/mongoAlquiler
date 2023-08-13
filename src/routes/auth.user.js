0254b72655ba087a19dd417e28e809855792f693
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-13 01:05:28 -0500

    feat(Root): Nuevas rutas
    Se añadieron nuevas rutas
    
    - /generate/:category : generar token especifico por categoria
    
    - clientes : obtener informacion de los clientes
    
    Cosas a tener en consideracion
    
    - Nuevos middlewares : Se añadieron nuevos middlewares en las rutas para el correcto funcionamiento de las validaciones y autenticaciones

--- src/routes/auth.user.js
+++ src/routes/auth.user.js
@@ -1,8 +1,46 @@
 import { Router } from "express";
 import { login } from "../controllers/auth.user.controller.js";
+import verifyDataMD from "../middleware/auth.login.md.js";
+import VTA from "../middleware/verifyjwtSession.js";
+import { numberRequest } from "../limits/setting.limits.js";
+import { generateTokenSpecific } from "../controllers/generate.structure.sign.token.js";
+import { customAuthClientPassport } from "../middleware/verifyTokens/jwt.clients.js";
+import searchAllClients from "../controllers/clients.findAll.js";
 
 const authUserRoute = Router();
 
-authUserRoute.get("/login", login)
+/**
+ * ? Route to generate token of session
+ * */
 
-export default authUserRoute; 
+authUserRoute.get(
+    "/login", 
+    numberRequest, 
+    verifyDataMD, 
+    login);
+
+/**
+ * ? Route to generate token of categories 
+ * */    
+
+authUserRoute.get(
+  "/generate/:category",
+  numberRequest,
+  VTA,
+  generateTokenSpecific,
+  (req, res) => res.send(req.category)
+);
+
+/**
+ * ? Route /clients
+ * */  
+
+authUserRoute.get(
+  "/clientes",
+  numberRequest,
+  VTA,
+  customAuthClientPassport,
+  searchAllClients
+);
+
+export default authUserRoute;
