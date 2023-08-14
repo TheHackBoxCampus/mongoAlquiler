5f615b9ba5f187c012b0be1efb633bf4569d7e1d
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-13 20:24:41 -0500

    feat(Root): Validacion general
    Mofique la funcion de jwtClient por jwtVerify  para validar todas las instancias de dto para los endpoints

--- src/middleware/verifyTokens/jwt.verify.js
+++ src/middleware/verifyTokens/jwt.verify.js
@@ -1,29 +1,29 @@
 import "reflect-metadata"
 import { classToPlain, plainToClass } from "class-transformer";
-import Client from "../../storage/structure/clients.js";
 import passport from "../../passport/auth.passport.setting.js"
 
-const verifyClientjwt = (decoded) => {
+const verifyjwt = (decoded, cls) => {
     let copy = JSON.stringify({...decoded}); 
-    let ref = JSON.stringify(classToPlain(plainToClass(Client, {}, {ignoreDecorators: true}))); 
+    let ref = JSON.stringify(classToPlain(plainToClass(cls, {}, {ignoreDecorators: true}))); 
     let verify = copy === ref; 
     return verify
 }
 
-const customAuthClientPassport = (req, res, next) => {
+const customAuthPassport = (cls) => (req, res, next) => {
     passport.authenticate("bearer", { session: false }, (err, decoded) => {
        if(err) res.status(500).send({status: 500, message: "Error en la autenticacion a clientes"})
        else {
           let np = Object.entries(decoded).map(p => p[0] != "iat" && p[0] != "exp" ? p : false).filter(Boolean)
           let it = Object.fromEntries(np);
-          let r = verifyClientjwt(it)
+          let r = verifyjwt(it, cls)
 
-          return r ? next() : res.status(500).send({status: 500, message: "El token que esta colocando no es de esta categoria"});
+          return r ? next() : res.status(500).send({status: 500, message: "token invalido o no es de esta categoria"});
   
         }
     })(req, res, next);
 }
 
 export {
-    customAuthClientPassport
+    customAuthPassport,
+    verifyjwt
 }; 
